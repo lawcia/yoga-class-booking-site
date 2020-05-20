6942725ab2fd5c597f703d9d633d70fa937f4461
lawcia <lawrencianjume@gmail.com>
2020-05-20 18:21:01 +0100

    create method to query db during form validation

--- yogabook/forms.py
+++ yogabook/forms.py
@@ -1,5 +1,7 @@
-from wtforms import SelectField, validators, SubmitField, SelectMultipleField, RadioField
+from wtforms import SelectField, validators, SubmitField, SelectMultipleField, RadioField, ValidationError
 from flask_wtf import FlaskForm
+from .models import Instructor
+from . import db
 
 class CreateInstructorForm(FlaskForm):
   name = SelectField('Name', choices=[('Alex Heart', 'Alex Heart'), ('Sam Doe', 'Sam Doe'), ('Noah Smith', 'Noah Smith'), ('Toyah Tyler', 'Toyah Tyler')])
@@ -24,3 +26,14 @@ class CreateInstructorForm(FlaskForm):
   pictures = RadioField('Picture', choices=[('/images/woman_in_sun.jpg', 'Picture 1'), ('/images/woman_hand_pose.jpg', 'Picture 2'), ('/images/man_sweater.jpg', 'Picture 3'), ('/images/woman_sunglasses.jpg', 'Picture 4')], default='/images/woman_in_sun.jpg')
   submit = SubmitField(label='Create Instructor')
   
+  def validate_name(self, field):
+    if Instructor.query.filter_by(name=field.data).first():
+      raise ValidationError('This name has been taken')
+  
+  def validate_phone(self, field):
+    if Instructor.query.filter_by(phone=field.data).first():
+      raise ValidationError('This phone number has been taken')
+  
+  def validate_email(self, field):
+    if Instructor.query.filter_by(email=field.data).first():
+      raise ValidationError('This email has been taken')
